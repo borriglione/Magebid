cae65968bddff236c99adf2ed97578b0c2d21c0c
unknown <worknice@.(none)>
2010-08-23 16:07:42 +0200

    Deleted folder "Abstract/" for Category and Store-Category Classes Added them in the auction/edit - view

--- app/code/local/Netresearch/Magebid/Block/Adminhtml/Profile/Edit/Tab/Category.php
+++ app/code/local/Netresearch/Magebid/Block/Adminhtml/Profile/Edit/Tab/Category.php
@@ -9,7 +9,7 @@
  * @link      http://www.magebid.de/
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
-class Netresearch_Magebid_Block_Adminhtml_Profile_Edit_Tab_Category extends Netresearch_Magebid_Block_Adminhtml_Abstract_Category_Tree
+class Netresearch_Magebid_Block_Adminhtml_Profile_Edit_Tab_Category extends Netresearch_Magebid_Block_Adminhtml_Auction_Edit_Tab_Category_Tree
 {
     /**
      * Construct
