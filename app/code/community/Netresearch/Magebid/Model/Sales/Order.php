d6e0060c4397ed89ebeff10485e37006340f0751
unknown <worknice@.(none)>
2010-09-03 18:16:09 +0200

    Order Feedback didn't work in Magento 1.3.2.4, Function _setState() doesn't exist in this Version. Added the modified function setState()

--- app/code/community/Netresearch/Magebid/Model/Sales/Order.php
+++ app/code/community/Netresearch/Magebid/Model/Sales/Order.php
@@ -17,6 +17,8 @@ class Netresearch_Magebid_Model_Sales_Order extends Mage_Sales_Model_Order
      * Dispatch Event, which is catched by Netresearch_Magebid_Model_Order_Observer to change the ebay status
      * By default allows to set any state. Can also update status to default or specified value
      * Сomplete and closed states are encapsulated intentionally, see the _checkState()
+     * 
+     * This function is for Magento >= Version 1.4.0.0
      *
      * @param string $state
      * @param string|bool $status
@@ -29,6 +31,23 @@ class Netresearch_Magebid_Model_Sales_Order extends Mage_Sales_Model_Order
     { 
      	$comment = Mage::getSingleton('magebid/order_status')->setEbayStatus($this,$state,$comment);  
     	return parent::_setState($state, $status, $comment, $isCustomerNotified, $shouldProtectState);
+    }
+    
+    /**
+     * Declare order state
+     *
+     * This function is for Magento < Version 1.4.0.0
+     *
+     * @param string $state
+     * @param string $status
+     * @param string $comment
+     * @param bool $isCustomerNotified
+     * @return  Mage_Sales_Model_Order
+     */
+    public function setState($state, $status = false, $comment = '', $isCustomerNotified = false)
+    {
+     	$comment = Mage::getSingleton('magebid/order_status')->setEbayStatus($this,$state,$comment);  
+    	return parent::setState($state, $status, $comment, $isCustomerNotified);
     }
 }
 ?>
