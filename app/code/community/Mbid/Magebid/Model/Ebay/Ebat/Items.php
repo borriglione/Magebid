906cf7f758f009b5b8757948bf57a8a68c12418f
Andre Herrn <info@magebid.com>
2011-04-29 14:50:06 +0200

    Fixed wrong encoding in ebay_ebat_items

--- app/code/community/Mbid/Magebid/Model/Ebay/Ebat/Items.php
+++ app/code/community/Mbid/Magebid/Model/Ebay/Ebat/Items.php
@@ -11,8 +11,8 @@ require_once 'EndItemRequestType.php';
  *
  * @category  Mbid
  * @package   Mbid_Magebid
- * @author    AndrÃ© Herrn <info@magebid.com>
- * @copyright 2010 AndrÃ© Herrn | Netresearch GmbH & Co.KG (http://www.netresearch.de)
+ * @author    André Herrn <info@magebid.com>
+ * @copyright 2010 André Herrn | Netresearch GmbH & Co.KG (http://www.netresearch.de)
  * @link      http://www.magebid.com/
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
